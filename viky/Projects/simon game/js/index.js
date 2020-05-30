7c6ec27b72341217f5eb39785b6d182569509475
04Vikyjangid <04viky@gmail.com>
2020-05-30 22:21:38 +0530

    my personal portfolio
    this is an attempt of building my personal portfolio

---
+++ viky/Projects/simon game/js/index.js
@@ -0,0 +1,47 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern =[];
+
+$(".btn").click(function() {
+
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
+
+  //console.log(userClickedPattern);
+  playSound(userChosenColour);
+
+});
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+
+ $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+ playSound(randomChosenColour);
+}
+
+function playSound(name){
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout (function (){
+   $("#" + currentColour).removeClass("pressed");
+  },100);
+
+}
